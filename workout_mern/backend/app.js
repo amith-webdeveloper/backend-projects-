983630615035d60195914a141806c9d03cafa21a
amith-webdeveloper <amithwebdev1840@gmail.com>
2024-08-15 17:39:11 +0530

    continue project

--- workout_mern/backend/app.js
+++ workout_mern/backend/app.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import noteModel from './models/note.model.js';
 const app = express()
-import noteModel from './models/note.model'
 
 
-app.get('/' , function(req , res){
-    res.send('hi')
+
+app.get('/' , async function(req , res){
+    try {
+        // throw Error
+        const notes = await noteModel.find().exec();
+        res.status(200).json(notes)
+        
+    } catch (error) {
+        console.log("something gone wrong");
+        res.status(500).json({err: error})
+        
+    }
 })
 
+
+app.use(function(err , req , res , next){
+
+
+})
 export default app;
